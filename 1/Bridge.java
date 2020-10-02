35100864caebed4f78b34bc6efec19f5ab1f73ed
Moosa Hassan <moosa7021@gmail.com>
2020-10-02 12:59:23 +1000

    mix of stack overflow / blackboard lectures right now

---
+++ 1/Bridge.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.Semaphore;
+
+public class Bridge
+{
+    private int crossed;
+    private Semaphore bridgeSem;
+
+    // constructor
+    public Bridge()
+    {
+        crossed = 0;
+        bridgeSem = new Semaphore(1);       // 1 bridge resource
+    }
+
+    // accessors
+    public int getCrossed()
+    {
+        return crossed;
+    }
+
+    // mutators
+    public void cross()
+    {
+        // semaphore acquire
+        try
+        {
+            bridgeSem.acquire();
+            crossed++;
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR: " + e);
+        }
+    }
+
+    public void exit()
+    {
+        // semaphore release
+        bridgeSem.release();
+    }
+}
--- 1/Farmer.java
+++ 1/Farmer.java
@@ -1,26 +1,64 @@
 import java.util.*;
 
-public class Farmer implements Runnable
+public class Farmer extends Thread
 {
     // attributes
     private String name;
+    private String island;
+    private String direction;
+    private Bridge cookStrait;
 
     // constructor
-    public Farmer(String n) {
+    public Farmer(String n, String l, String d, Bridge b) 
+    {
         this.name = n;
-        r.nextInt(999);
+        this.island = l;
+        updateDirection();
+        this.cookStrait = b;
+        System.out.println(name + ": Waiting for bridge. Going towards " + direction);
     }
 
     // mutators
-    public void setName(String n)
+    public void setFarmerName(String n)
     {
         this.name = n;
     }
+    public void setIsland(String l)
+    {
+        this.island = l;
+    }
+    public void setDirection(String d)
+    {
+        this.direction = d;
+    }
 
     // accessors
-    public String getName()
+    public String getFarmerName()
     {
         return this.name;
+    }
+    public String getIsland()
+    {
+        return this.island;
+    }
+    public String getDirection()
+    {
+        return this.direction;
+    }
+
+    // methods
+    public void updateDirection()
+    {
+        // farmer is on NORTH island
+        if(this.island.equals("North"))
+        {
+            this.direction = "South";
+        }
+        // farmer is on SOUTH island
+        else
+        {
+            this.direction = "North";
+        }
     }
 
     public void run()
